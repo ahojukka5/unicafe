c7927cb0b84b7886310f6e05b10f775b39e755d4
ahojukka5 <ahojukka5@users.noreply.github.com>
2020-12-27 04:20:25 +0000

    deploy: 90e7f6cf2d1b7d9fcc19ae48c4c6209d153598f9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a4162ef844888c7417e933f97ce5c50.js"
+  "/unicafe/precache-manifest.4e3f9719195fa4c9a59c8de8cc81a10e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unicafe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
